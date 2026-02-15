21494a167e251bc8e33ec94297c747f12bee63e6
Joel Cutler <joelcutler108@gmail.com>
2026-02-15 01:07:53 +0000

    get releases list from the latest afp

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use std::{collections::HashMap, fmt::format};
+
 use anyhow::Context;
 use regex::Regex;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct AFPRepo {
+    id: i32,
     name: String,
 }
 
-async fn get_latest_afp_repo_name() -> anyhow::Result<String> {
-    let isa_afp_repo_list_endpoint = "https://foss.heptapod.net/api/v4/groups/isa-afp/projects?order_by=last_activity_at&sort=desc";
+async fn get_afp_repos() -> anyhow::Result<Vec<AFPRepo>> {
+    let afp_repo_list_url =
+        "https://foss.heptapod.net/api/v4/groups/isa-afp/projects?order_by=last_activity_at&sort=desc";
 
     let client = reqwest::Client::new();
-    let projects: Vec<AFPRepo> = client
-        .get(isa_afp_repo_list_endpoint)
+    let repos: Vec<AFPRepo> = client
+        .get(afp_repo_list_url)
         .header("User-Agent", "belle-client")
         .send()
         .await
@@ -21,22 +25,50 @@ async fn get_latest_afp_repo_name() -> anyhow::Result<String> {
         .await
         .context("Failed to parse JSON response from Hetapod")?;
 
-    let re =
-        Regex::new(r"^afp-[\d-]+$").context("Invalid regex pattern for AFP repository name")?;
-    let latest_afp = projects
-        .into_iter()
-        .find(|p| re.is_match(&p.name))
-        .context("No latest AFP repository could be found")?;
+    let re = Regex::new(r"^afp-[\d-]+$").context("Invalid regex pattern for AFP repository name")?;
+    let afp_repos: Vec<AFPRepo> = repos.into_iter().filter(|p| re.is_match(&p.name)).collect();
 
-    return Ok(latest_afp.name);
+    return Ok(afp_repos);
 }
+
+type ReleaseMap = HashMap<String, HashMap<String, String>>;
+
+async fn get_releases(repo: &AFPRepo) -> anyhow::Result<ReleaseMap> {
+    let releases_file_url = format!(
+        "https://foss.heptapod.net/api/v4/projects/{}/repository/files/metadata%2Freleases.toml/raw",
+        repo.id
+    );
+
+    let client = reqwest::Client::new();
+    let releases_content = client
+        .get(releases_file_url)
+        .header("User-Agent", "belle-client")
+        .send()
+        .await
+        .with_context(|| format!("Failed to fetch releases file for '{}' repo", repo.name))?
+        .text()
+        .await
+        .with_context(|| format!("Failed to read releases file for '{}' repo", repo.name))?;
+
+    let releases = toml::from_str(&releases_content)
+        .with_context(|| format!("Failed to parse TOML from releases file for '{}' repo", repo.name))?;
+
+    Ok(releases)
+}
+
+async fn get_packages_meta(releases: ReleaseMap) {}
+// https://foss.heptapod.net/api/v4/projects/{}/repository/archive.zip?path=metadata/entries
+
+// todo get dependencies through ROOT files (this will be more difficult)
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let name = get_latest_afp_repo_name().await?;
-    print!("name: {}", name);
+    let afp_repos = get_afp_repos().await?;
+    let latest_repo = afp_repos.first().context("No latest AFP repository could be found")?;
+    println!("name: {} {}", latest_repo.name, latest_repo.id);
+
+    let releases = get_releases(latest_repo).await?;
+    println!("{:#?}", releases);
 
     return Ok(());
 }
-
-// curl -L -o releases.toml https://foss.heptapod.net/isa-afp/afp-2025-2/-/raw/branch/default/metadata/releases.toml
