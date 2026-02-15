d468ba79a74563631a584e6a871f9a0b05a7dabd
Joel Cutler <joelcutler108@gmail.com>
2026-02-15 20:06:41 +0000

    create global config holder

---
+++ src/config/mod.rs
@@ -0,0 +1,47 @@
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    sync::OnceLock,
+};
+
+use anyhow::Context;
+use serde::Deserialize;
+
+pub static CONFIG: OnceLock<BelleConfig> = OnceLock::new();
+
+#[derive(Deserialize)]
+pub struct BelleConfig {
+    root_dir: PathBuf,
+}
+
+impl BelleConfig {
+    pub fn init() -> anyhow::Result<()> {
+        let config_path = env::var("BELLE_CONFIG").unwrap_or(String::from("./belle_config.toml"));
+        let config_file = Path::new(&config_path);
+
+        let parsed: BelleConfig = if config_file.is_file() {
+            let content = fs::read_to_string(config_file)
+                .with_context(|| format!("Failed to read config file at '{}'", config_path))?;
+            toml::from_str(&content)
+                .with_context(|| format!("Failed to parse TOML config file at '{}'", config_path))?
+        } else {
+            BelleConfig::default()
+        };
+
+        CONFIG.set(parsed);
+        return Ok(());
+    }
+
+    pub fn global() -> &'static Self {
+        return CONFIG.get().expect("Config was not initialised");
+    }
+
+    fn default() -> Self {
+        // ? how do I handle linux differently
+        let base_path = env::var("APPDATA").map(PathBuf::from).unwrap();
+
+        return Self {
+            root_dir: base_path.join("belle"),
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use crate::config::BelleConfig;
+
+mod config;
 mod fetch;
+mod registry;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    BelleConfig::init()?;
+
     fetch::update().await?;
 
     return Ok(());
