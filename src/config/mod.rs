1ec1b1a2468bcc25d305620f5b494938a75d0c7b
Joel Cutler <joelcutler108@gmail.com>
2026-02-17 00:28:14 +0000

    add comments to config manager

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -15,10 +15,13 @@ pub struct BelleConfig {
 }
 
 impl BelleConfig {
+    /// Initialise config, this must only be called once
     pub fn init() -> anyhow::Result<()> {
+        // Load config file from location at environment variable `BELLE_CONFIG` or check the executables location if that is not set
         let config_path = env::var("BELLE_CONFIG").unwrap_or(String::from("./belle_config.toml"));
         let config_file = Path::new(&config_path);
 
+        // Use default values if the config is not found
         let parsed: BelleConfig = if config_file.is_file() {
             let content = fs::read_to_string(config_file)
                 .with_context(|| format!("Failed to read config file at '{}'", config_path))?;
@@ -28,16 +31,20 @@ impl BelleConfig {
             BelleConfig::default()
         };
 
-        CONFIG.set(parsed);
+        CONFIG.set(parsed).expect("Config has already been initialised");
         return Ok(());
     }
 
+    /// Return the global configuration, this must be called after `init`
     pub fn global() -> &'static Self {
+        // This will panic if `init()` was not called, so we must ensure this was done in our entry point
         return CONFIG.get().expect("Config was not initialised");
     }
 
+    /// Defaults for config
     fn default() -> Self {
-        // ? how do I handle linux differently
+        // todo handle linux differently
+        // Default root directory under the user's application data
         let base_path = env::var("APPDATA").map(PathBuf::from).unwrap();
 
         return Self {
@@ -45,3 +52,5 @@ impl BelleConfig {
         };
     }
 }
+
+// todo writing to config
