3fcddbb03b1b56c9c1ea23c5c5a88fbd30c5027c
Joel Cutler <joelcutler108@gmail.com>
2026-03-07 21:40:05 +0000

    allow registering component though isabelle representation

--- src/isabelle/isabelle.rs
+++ src/isabelle/isabelle.rs
@@ -6,23 +6,20 @@ use std::{
 
 use anyhow::Context;
 
-use crate::{isabelle::types::Isabelle, util::get_isabelle_version};
+use crate::{config::BelleConfig, isabelle::types::Isabelle, util::get_isabelle_version};
 
 impl Isabelle {
     pub fn locate(path: PathBuf) -> anyhow::Result<Self> {
-        let res = Self::exec_isabelle(&path, "version")?;
-        let res_str = String::from_utf8(res.stdout).context("Isabelle command output was not valid UTF-8")?;
-        let version = get_isabelle_version(&res_str);
+        let res = Self::exec_isabelle_from_path(&path, "isabelle version")?;
+        let version = get_isabelle_version(&res);
 
         return Ok(Self { version, path });
     }
 
-    fn exec_isabelle(isabelle_root: &PathBuf, cmd: &str) -> anyhow::Result<Output> {
+    fn exec_isabelle_from_path(isabelle_root: &PathBuf, cmd: &str) -> anyhow::Result<String> {
         // todo unix-ify a variant
         let bash = isabelle_root.join("contrib").join("cygwin").join("bin").join("bash.exe");
         let isabelle_bin = isabelle_root.join("bin");
-
-        let isabelle_cmd = format!("isabelle {}", cmd);
 
         // Create a command using defaults from `Cygwin-Terminal.bat`
         let mut command = Command::new(bash);
@@ -40,12 +37,47 @@ impl Isabelle {
             .env("CHERE_INVOKING", "true")
             .arg("--login")
             .arg("-c")
-            .arg(&isabelle_cmd);
+            .arg(cmd);
 
         let res = command
             .output()
-            .with_context(|| format!("Failed to execute Isabelle command {}", isabelle_cmd))?;
+            .with_context(|| format!("Failed to execute Isabelle command '{}'.", cmd))?;
 
-        return Ok(res);
+        let res_str = String::from_utf8(res.stdout)
+            .with_context(|| format!("Isabelle command output for '{}' was not valid UTF-8.", cmd))?;
+
+        return Ok(res_str);
+    }
+
+    fn exec_isabelle(&self, cmd: &str) -> anyhow::Result<String> {
+        return Self::exec_isabelle_from_path(&self.path, cmd);
+    }
+
+    pub fn get_isabelle_path(&self, path: PathBuf) -> anyhow::Result<String> {
+        // todo unix-ify a variant
+        let path = self.exec_isabelle(&format!("cygpath -u {}", path.to_string_lossy().to_string()))?;
+
+        return Ok(path.trim().to_string());
+    }
+
+    fn manage_component(&self, add: bool) -> anyhow::Result<()> {
+        let active_env_dir = BelleConfig::read_config(|c| c.get_active_env_link());
+        let isabelle_path = self.get_isabelle_path(active_env_dir)?;
+
+        // Add or remove the active environment directory as a component to isabelle
+        let flag = if add { "-u" } else { "-x" };
+        self.exec_isabelle(&format!("isabelle components {} {}", flag, isabelle_path))?;
+
+        return Ok(());
+    }
+
+    pub fn add_component(&self) -> anyhow::Result<()> {
+        self.manage_component(true)?;
+        return Ok(());
+    }
+
+    pub fn exclude_component(&self) -> anyhow::Result<()> {
+        self.manage_component(false)?;
+        return Ok(());
     }
 }
