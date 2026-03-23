45dd4b7c17ae00a95a3c44d0efea85d48ef34755
Joel Cutler <joelcutler108@gmail.com>
2026-03-23 15:33:24 +0000

    move hinted/advice error wrapper into new crate

---
+++ hinted/src/lib.rs
@@ -0,0 +1,30 @@
+pub trait Hint {
+    fn get_hint(&self) -> Option<&'static str>;
+}
+
+use std::borrow::Cow;
+mod hinted;
+
+pub use hinted::{HintContainerExt, HintedResultExt};
+pub use hinted_derive::Hint;
+
+/// Wrapper storing a hints for enum
+#[derive(Debug)]
+pub struct Hinted<E> {
+    source: E,
+    hint: Option<Cow<'static, str>>,
+}
+
+impl<E: Hint> Hinted<E> {
+    /// Retrieve hint for given enum
+    pub fn hint(&self) -> Option<Cow<'static, str>> {
+        // Priorities custom advice, then use macro set error, then none
+        self.hint.clone().or_else(|| self.source.get_hint().map(Cow::Borrowed))
+    }
+}
+
+impl<E> Hinted<E> {
+    pub fn source(&self) -> &E {
+        &self.source
+    }
+}
