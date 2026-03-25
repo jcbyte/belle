8a481f517f8da4207655e7f8d432714fd5a17c1a
Joel Cutler <joelcutler108@gmail.com>
2026-03-25 18:05:21 +0000

    test isabelle module

--- src/isabelle/path.rs
+++ src/isabelle/path.rs
@@ -6,6 +6,11 @@ pub trait IsabellePathContext {
 
 impl IsabellePathContext for Path {
     fn to_isabelle_path(&self) -> Option<String> {
+        // Ensure an absolute path is passed
+        if !self.is_absolute() {
+            return None;
+        }
+
         let path_str = self.to_str()?;
 
         if cfg!(windows) {
@@ -17,5 +22,40 @@ impl IsabellePathContext for Path {
 
         // On linux, this is expected by isabelle
         Some(path_str.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_rejects_relative_path() {
+        let path = Path::new("relative/path/to/file");
+        assert_eq!(path.to_isabelle_path(), None);
+    }
+
+    #[cfg(not(windows))]
+    #[test]
+    fn test_linux_absolute_path() {
+        let path = Path::new("/home/user/isabelle");
+        let isabelle_path = path.to_isabelle_path();
+        assert!(isabelle_path.is_some());
+        assert_eq!(isabelle_path.unwrap(), "/home/user/isabelle");
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn test_windows_cygpath_conversion() {
+        let path = Path::new(r"C:\Users\Guest\Project");
+        let isabelle_path = path.to_isabelle_path();
+        assert!(isabelle_path.is_some());
+        assert_eq!(isabelle_path.unwrap(), "/cygdrive/c/Users/Guest/Project");
+
+        let path = Path::new(r"D:\Data");
+        let isabelle_path = path.to_isabelle_path();
+        assert!(isabelle_path.is_some());
+        assert_eq!(isabelle_path.unwrap(), "/cygdrive/d/Data");
     }
 }
